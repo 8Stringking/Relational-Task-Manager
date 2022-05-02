1b9f9e4b28c268d8a4a9bce949900d2623c1bc37
8Stringking <tomcondemned@hotmail.co.uk>
2022-05-02 20:23:42 +0000

    added add task functionality

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,15 @@ document.addEventListener('DOMContentLoaded', function() {
     // sidenav initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker initialisation
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker,{
+        format: "dd mm, yyyy",
+        i18n: {done: "select"}
+    });
+
+    // select initialisation
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
   });
